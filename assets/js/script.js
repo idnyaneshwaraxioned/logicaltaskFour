27e11c3414f3370e5d8abed6016b76ef921569d4
idnyaneshwaraxioned <dnyaneshwari@prdxn.com>
2021-04-05 13:02:13 +0530

    Add ID field in table

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,9 +17,10 @@ var xhr = new XMLHttpRequest();
     function responseData() {
         if (xhr.readyState === 4 && xhr.status === 200) {
             var response_data = JSON.parse(xhr.responseText);
+            // console.log(response_data);
             var tableData = " ";
             for (var i = 0; i < n; i++) {
-                tableData += "<tr><td>" + response_data[i].title +
+                tableData += "<tr><td>"+ response_data[i].id +"</td><td>" + response_data[i].title +
                     "</td><td>" + response_data[i].body +
                     "</td></tr>";
             }
